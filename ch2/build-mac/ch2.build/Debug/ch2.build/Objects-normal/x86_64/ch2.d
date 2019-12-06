593d3f16645f08c6b2c868c0f1979e0dd265eced
linmengran <cqulinmengran@gmail.com>
2019-12-06 17:26:25 +0800

    neaten ch2 ch3 project

--- ch2/build-mac/ch2.build/Debug/ch2.build/Objects-normal/x86_64/ch2.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/linmengran-mac2/work/2.code_practice/is-practice/ch2/src/ch2.cpp \
-  /Users/linmengran-mac2/work/2.code_practice/is-practice/ch2/head/ch2.h
--- ch2/build-mac/ch2.build/Debug/ch2.build/Objects-normal/x86_64/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/linmengran-mac2/work/2.code_practice/is-practice/ch2/main.cpp \
-  /Users/linmengran-mac2/work/2.code_practice/is-practice/ch2/head/ch2.h
